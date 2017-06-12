f61215a72a1e933d6129d88b242e01aba029af27
AurelienBernard34 <aurelienbernard34@gmail.com>
2017-06-12 12:01:03 +0200

    premier tp

--- app/src/main/java/com/example/admin/tpandroid2/MainActivity.java
+++ app/src/main/java/com/example/admin/tpandroid2/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.admin.tpandroid2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Formation","premier log");
         setContentView(R.layout.activity_main);
     }
 }
